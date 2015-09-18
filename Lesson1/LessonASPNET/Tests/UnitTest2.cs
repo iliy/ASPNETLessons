204e580ab4ac598934d600cd9e687fc6592a786e
iliy <noboss1992@gmail.com>
2015-09-18 11:14:23 +0300

    Добавление нового модульного теста с использованием Moq объекта.

--- Lesson1/LessonASPNET/Tests/UnitTest2.cs
+++ Lesson1/LessonASPNET/Tests/UnitTest2.cs
@@ -28,5 +28,44 @@ namespace Tests
 
             Assert.AreEqual(result, products.Sum(p => p.Price));
         }
+
+        private Product[] createProduct(decimal price)
+        {
+            return new[] { new Product { Price = price } };
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void Pass_Through_Variable_Discounts()
+        {
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            // Условие, если ничего другое не подходит
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>()))
+                .Returns<decimal>(total => total);
+            // Условие, если переданное значение не является положительным
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(p => p <= 0)))
+                .Throws<System.ArgumentOutOfRangeException>();
+            // Условие, если переданное значение больше 100
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(p => p > 100)))
+                .Returns<decimal>(total => total * 0.9m);
+            // Условие, если переданное значение входит в интервал от 10 до 100 включительно
+            mock.Setup(m => m.ApplyDiscount(It.IsInRange<decimal>(10, 100, Range.Inclusive)))
+                .Returns<decimal>(total => total - 5);
+
+            var target = new LinqValueCalculator(mock.Object);
+
+            decimal FiveDollarsDiscount = target.ValueProducts(createProduct(5));
+            decimal TenDollarsDiscount = target.ValueProducts(createProduct(10));
+            decimal FiftenDollarsDiscount = target.ValueProducts(createProduct(50));
+            decimal OneHandredDollarsDiscount = target.ValueProducts(createProduct(100));
+            decimal TwoHandredDollarsDiscount = target.ValueProducts(createProduct(200));
+
+            Assert.AreEqual(5, FiveDollarsDiscount, "Five dollars discount wrong");
+            Assert.AreEqual(5, TenDollarsDiscount, "Five dollars discount wrong");
+            Assert.AreEqual(45, FiftenDollarsDiscount, "Five dollars discount wrong");
+            Assert.AreEqual(95, OneHandredDollarsDiscount, "Five dollars discount wrong");
+            Assert.AreEqual(180, TwoHandredDollarsDiscount, "Five dollars discount wrong");
+            target.ValueProducts(createProduct(0));
+        }
     }
 }
