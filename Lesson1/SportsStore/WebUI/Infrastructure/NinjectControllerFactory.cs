6e171f12ca75dc1f7b0ccb159b367071fb1edd86
iliy <noboss1992@gmail.com>
2015-09-18 13:06:01 +0300

    Добавлена своя фабрика для создания контроллеров

---
+++ Lesson1/SportsStore/WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Mvc;
+using Ninject;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectControllerFactory:DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            // Конфигурирование контейнера IKernel
+        }
+    }
+}
